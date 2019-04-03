93dd4538318d4e8a565965791a9e583ab3a1333e
Ryan Comerford <ryncmrfrd@users.noreply.github.com>
2019-04-03 12:35:03 +1100

    Cleaning Up

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Lifx Web Controller
-A nice looking, easy to use Lifx controller.
+# Lifx Web Controller :bulb:
+A nice looking, easy to use Lifx controller for the web.
+
+## Use
+Just go [here](http://lifx.ryncmrfrd.me) and login using your auth code.
+> You need one of these because LIFX won't let me use OAuth to login :face-concerned:. To get an auth token in your [Account Settings](https://cloud.lifx.com/settings).
+It does actually work. Just Trust me.
--- js/app.js
+++ js/app.js
@@ -232,3 +232,44 @@ $('.brightness').change(function() {
         'power': 'on'
     })
 });
+
+function colorTemperature2rgb(kelvin) {
+    var temperature = kelvin / 100.0;
+    var red, green, blue;
+    if (temperature < 66.0) {
+        red = 255
+    } else {
+        red = temperature - 55.0;
+        red = 351.97690566805693 + 0.114206453784165 * red - 40.25366309332127 * Math.log(red);
+        if (red < 0) red = 0;
+        if (red > 255) red = 255
+    }
+    if (temperature < 66.0) {
+        green = temperature - 2;
+        green = -155.25485562709179 - 0.44596950469579133 * green + 104.49216199393888 * Math.log(green);
+        if (green < 0) green = 0;
+        if (green > 255) green = 255
+    } else {
+        green = temperature - 50.0;
+        green = 325.4494125711974 + 0.07943456536662342 * green - 28.0852963507957 * Math.log(green);
+        if (green < 0) green = 0;
+        if (green > 255) green = 255
+    }
+    if (temperature >= 66.0) {
+        blue = 255
+    } else {
+        if (temperature <= 20.0) {
+            blue = 0
+        } else {
+            blue = temperature - 10;
+            blue = -254.76935184120902 + 0.8274096064007395 * blue + 115.67994401066147 * Math.log(blue);
+            if (blue < 0) blue = 0;
+            if (blue > 255) blue = 255
+        }
+    }
+    return {
+        red: Math.round(red),
+        blue: Math.round(blue),
+        green: Math.round(green)
+    }
+}
